f59fc243e7f610bb26515af58427b2eeb60349e2
Abhishek Shaw <145495694+Abhishek26102002@users.noreply.github.com>
2025-01-07 00:07:36 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -25,7 +25,7 @@ const Login = () => {
       return handelError("email and password are required");
     }
     try {
-      const url = `https://mern-02-authentication-backend.vercel.app`;
+      const url = `https://mern-02-authentication-backend.vercel.app/login`;
       const response = await fetch(url, {
         method: "POST",
         headers: {
